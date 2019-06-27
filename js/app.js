0089a800484396fc939a9f1488c31ce19413aeac
Jimmy Weng <opep0000@gmail.com>
2019-06-27 10:20:13 +0800

    Add a list containing multiple dogs

--- js/app.js
+++ js/app.js
@@ -10,31 +10,54 @@ let dogs = [
       imgSrc : 'img/dog2.jpg',
       clickCount: 0
     },
+    {
+      name : 'Rari',
+      imgSrc : 'img/dog3.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Baru',
+      imgSrc : 'img/dog4.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Wagen',
+      imgSrc : 'img/dog5.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Porsche',
+      imgSrc : 'img/dog6.jpg',
+      clickCount : 0
+    }
 ];
 
 
 /* ======= View ======= */
-const main = document.querySelector('.main');
-main.innerHTML = "";
+// Create a list of several dogs
+const dogList = document.querySelector('.list');
 
-
-// Loop over the dogs
 for (const dog of dogs) {
-    let name = document.createElement('h2');
-    let image = document.createElement('img');
-    let count = document.createElement('div');
+    // Use dog name as the context of the listItem
+    let listItem = document.createElement('li');
+    listItem.textContent = dog.name;
 
-    name.textContent = dog.name;
-    image.src = dog.imgSrc;
-    count.textContent = `Count: ${dog.clickCount}`;
-
-    main.appendChild(name);
-    main.appendChild(image);
-    main.appendChild(count);
-
-    // Click Event
-    image.addEventListener('click', function() {
-        dog.clickCount += 1;
-        count.textContent = `Count: ${dog.clickCount}`;
-    });
+    // append each item to the list
+    dogList.appendChild(listItem);
 }
+
+// Set the initial dog
+let showingDog = dogs[0];
+
+let name = document.querySelector('#dogName');
+let image = document.querySelector('img');
+let count = document.querySelector('.count');
+
+name.textContent = showingDog.name;
+image.src = showingDog.imgSrc;
+count.textContent = `Count: ${showingDog.clickCount}`;
+
+image.addEventListener('click', function() {
+    showingDog.clickCount += 1;
+    count.textContent = `Count: ${showingDog.clickCount}`;
+});
