a0893d214f232fef0bfe30c34e629dc746530ba8
Jimmy Weng <opep0000@gmail.com>
2019-06-27 14:59:53 +0800

    Reorganize the code by seperating model and view in app.js

--- js/app.js
+++ js/app.js
@@ -1,75 +1,123 @@
 /* ======= Model ======= */
-let dogs = [
-    {
-      name: 'Yotta',
-      imgSrc : 'img/dog1.jpg',
-      clickCount: 0
+let model = {
+    showingDog: null,
+    dogs: [
+        {
+          name: 'Yotta',
+          imgSrc : 'img/dog1.jpg',
+          clickCount: 0
+        },
+        {
+          name: 'Zda',
+          imgSrc : 'img/dog2.jpg',
+          clickCount: 0
+        },
+        {
+          name : 'Rari',
+          imgSrc : 'img/dog3.jpg',
+          clickCount : 0
+        },
+        {
+          name : 'Baru',
+          imgSrc : 'img/dog4.jpg',
+          clickCount : 0
+        },
+        {
+          name : 'Wagen',
+          imgSrc : 'img/dog5.jpg',
+          clickCount : 0
+        },
+        {
+          name : 'Porsche',
+          imgSrc : 'img/dog6.jpg',
+          clickCount : 0
+        }
+    ]
+};
+
+
+/* ======= Octopus ======= */
+let octopus = {
+    init() {
+        model.showingDog = model.dogs[0];
+        display.init();
+        list.init();
     },
-    {
-      name: 'Zda',
-      imgSrc : 'img/dog2.jpg',
-      clickCount: 0
+
+    getDog() {
+        return model.showingDog;
     },
-    {
-      name : 'Rari',
-      imgSrc : 'img/dog3.jpg',
-      clickCount : 0
+
+    switchDog(dog) {
+        model.showingDog = dog;
     },
-    {
-      name : 'Baru',
-      imgSrc : 'img/dog4.jpg',
-      clickCount : 0
+
+    getList() {
+        return model.dogs;
     },
-    {
-      name : 'Wagen',
-      imgSrc : 'img/dog5.jpg',
-      clickCount : 0
-    },
-    {
-      name : 'Porsche',
-      imgSrc : 'img/dog6.jpg',
-      clickCount : 0
+
+    // increments the counter for the currently-selected cat
+    incrementCounter() {
+        model.showingDog.clickCount++;
+        display.render();
     }
-];
+};
 
 
 /* ======= View ======= */
-// Set the initial dog
-let showingDog = dogs[0];
+let display = {
+    init() {
+      // store pointers to our DOM elements
+      this.name = document.querySelector('#dogName');
+      this.count = document.querySelector('.count');
+      this.image = document.querySelector('img');
 
-let name = document.querySelector('#dogName');
-let image = document.querySelector('img');
-let count = document.querySelector('.count');
+      // on click, increment the current dog's counter
+      this.image.addEventListener('click', function() {
+          octopus.incrementCounter();
+      });
+      // render this view (update the DOM elements with the right values)
+      this.render();
+    },
 
-// update the DOM elements with showing dog
-function render() {
-    name.textContent = showingDog.name;
-    image.src = showingDog.imgSrc;
-    count.textContent = `Count: ${showingDog.clickCount}`;
-}
+    render() {
+      // update the DOM elements with values from the display dog
+      let displayDog = octopus.getDog();
+      this.name.textContent = displayDog.name;
+      this.image.src = displayDog.imgSrc;
+      this.count.textContent = `Count: ${displayDog.clickCount}`;
+    }
+};
 
-image.addEventListener('click', function() {
-    // increments the counter for the showing dog
-    showingDog.clickCount ++;
-    render();
-});
+let list = {
+    // store the DOM element
+    init() {
+        this.dogList = document.querySelector('.list');
 
-render();
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
 
-// Create a list of several dogs
-const dogList = document.querySelector('.list');
+    render() {
+        // get the dogs from the octopus
+        let dogs = octopus.getList();
+        // loop over the dogs
+        for (const dog of dogs) {
+            // Use dog name as the context of the listItem
+            let listItem = document.createElement('li');
+            listItem.textContent = dog.name;
 
-for (const dog of dogs) {
-    // Use dog name as the context of the listItem
-    let listItem = document.createElement('li');
-    listItem.textContent = dog.name;
+            // When listItem got clicked, switch the showing dog
+            listItem.addEventListener('click', function() {
+                octopus.switchDog(dog);
+                display.render();
+            });
 
-    // Whem listItem got clicked, switch the showing dog
-    listItem.addEventListener('click', function() {
-        showingDog = dog;
-        render();
-    });
+            // append each item to the list
+            this.dogList.appendChild(listItem);
+        }
+    }
+};
 
-    // append each item to the list
-    dogList.appendChild(listItem);
-}
+// run the app
+octopus.init();
